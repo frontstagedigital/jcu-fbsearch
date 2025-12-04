d8a6c120c995f42d9b30b9037bad3fad1f6289c3
Aleks Bochniak <aleks@webisbeautiful.com>
2025-12-04 14:09:33 +0000

    New pagination markup

--- pagination/pagination.js
+++ pagination/pagination.js
@@ -45,33 +45,36 @@ var Pager = (function () {
 
     var b = Html.buffer();
     b.add('<div class="container">');
-    b.add('  <div class="flex align-center space-center">');
+    b.add('  <ol class="list-none p-l-0 flex flex-wrap align-center space-center">');
 
     // Prev
     if (currentPage > 1) {
       var prevStart = Math.max(1, currentStart - size);
-      b.add('    <a class="external-disabled" href="' + Html.esc(hrefForStart(qs, prevStart)) + '">');
-      b.add('      <button class="m-r-050 special-three border-none round-xs p-t-050 p-b-050 p-l-100 p-r-100 m-b-150 f-overline">Prev</button>');
-      b.add('    </a>');
+      b.add('    <li>');
+      b.add('      <a class="m-r-050 btn secondary-one border-none p-t-050 p-b-050 p-l-100 p-r-100 m-b-150 f-overline external-disabled" href="' + Html.esc(hrefForStart(qs, prevStart)) + '">Prev</a>');
+      b.add('    </li>');
     }
 
     // Numbered pages
     for (var p = startPage; p <= endPage; p++) {
       var startRank = (p - 1) * size + 1;
-      b.add('    <a class="external-disabled" href="' + Html.esc(hrefForStart(qs, startRank)) + '">');
-      b.add('      <button class="m-r-0125 special-three border-none round-xs p-t-050 p-b-050 p-l-100 p-r-100 m-b-150 f-overline">' + p + '</button>');
-      b.add('    </a>');
+      var isActive = (p === currentPage);
+      var cls = 'd-none-sm m-r-0125 btn secondary-one border-none p-t-050 p-b-050 p-l-100 p-r-100 m-b-150 f-overline external-disabled' + (isActive ? ' active' : '');
+      var aria = isActive ? ' aria-current="page"' : '';
+      b.add('    <li>');
+      b.add('      <a class="' + cls + '" href="' + Html.esc(hrefForStart(qs, startRank)) + '"' + aria + '>' + p + '</a>');
+      b.add('    </li>');
     }
 
     // Next
     if (currentPage < totalPages) {
       var nextStart = currentStart + size;
-      b.add('    <a class="external-disabled" href="' + Html.esc(hrefForStart(qs, nextStart)) + '">');
-      b.add('      <button class="special-three border-none round-xs p-t-050 p-b-050 p-l-100 p-r-100 m-b-150 f-overline">Next</button>');
-      b.add('    </a>');
+      b.add('    <li>');
+      b.add('      <a class="btn secondary-one border-none p-t-050 p-b-050 p-l-100 p-r-100 m-b-150 f-overline external-disabled" href="' + Html.esc(hrefForStart(qs, nextStart)) + '">Next</a>');
+      b.add('    </li>');
     }
 
-    b.add('  </div>');
+    b.add('  </ol>');
     b.add('</div>');
     return b.toString();
   }
