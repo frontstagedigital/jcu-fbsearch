90297f178fe4de79fa17bdc8cd6f84f75d54261a
Aleks Bochniak <aleks@webisbeautiful.com>
2025-12-09 12:11:05 +0000

    debug

--- render/render.js
+++ render/render.js
@@ -424,6 +424,11 @@ var FeaturedFilters = (function () {
       }
     }
 
+    if(facetSlug === "study-area" && values.length > 14) {
+        b.add('</div">');
+    }
+
+
     if (isMulti) {
       b.add('      <section class="search-filter-buttons flex-shrink-0 col-12">');
       b.add('        <div class="flex gap-050 p-t-100 p-l-050 p-r-050 p-b-050">');
@@ -431,10 +436,6 @@ var FeaturedFilters = (function () {
       b.add('          <button class="cancel-button btn secondary-two m-b-0 w-100">Cancel</button>');
       b.add('        </div>');
       b.add('      </section>');
-    }
-
-    if(facetSlug === "study-area" && values.length > 14) {
-        b.add('</div">');
     }
 
     b.add('    </div>'); // inner
