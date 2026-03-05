375002440931e8ae2b3e75a833fea4ea52fedf28
Alex Foy <foyalex@gmail.com>
2026-03-05 13:27:04 +1300

    cater for undefined salespitch fields

--- render/render.js
+++ render/render.js
@@ -370,7 +370,7 @@ var Results = (function () {
 
   function description150(result) {
     var md = (result && result.listMetadata) || {};
-    var salesPitch = (intOnlyFlag === true) ? md.salesPitchInternational : md.salesPitchDomestic;
+    var salesPitch = firstNonEmptyArray((intOnlyFlag === true) ? md.salesPitchInternational : md.salesPitchDomestic);
     var cArr = firstNonEmptyArray(md.c);
     var fromC = salesPitch.length ? String(salesPitch[0]).trim() : (cArr.length ? String(cArr[0]).trim() : "");
 
